fe0aceaa8205fd173c6921ca931c3f1838227d1a
zhonghuawu <wu_zhonghua@outlook.com>
2015-11-22 20:25:01 +0800

    add decision_tree

--- Data_Mining_DT/decision_tree/main.cpp
+++ Data_Mining_DT/decision_tree/main.cpp
@@ -8,23 +8,8 @@
 #include "utility.h"
 
 const static long TOTAL_LINE = 494021;
-//const static long ATTRIBUTION_NUM = 41;
+const static long ATTRIBUTION_NUM = 41;
 const static double LOG2 = 0.693147;
-
-/***
- * 计算连续属性中最大值和最小值
- */
-void max_min(vector<double>::const_iterator beg,
-		vector<double>::const_iterator end, double *maxVal, double *minVal) {
-	*maxVal = *beg;
-	*minVal = *beg;
-	for (vector<double>::const_iterator iter = beg; iter != end; iter++) {
-		if (*maxVal < *iter)
-			*maxVal = *iter;
-		if (*minVal > *iter)
-			*minVal = *iter;
-	}
-}
 
 /***
  * 计算熵
@@ -50,8 +35,10 @@ void calcPro(vector<string>::const_iterator beg,
 		pro_label->push_back(pro);
 	}
 }
+//计算一个属性的熵值
 double calcOneAttrEntropy(vector<string>::const_iterator beg_attr,
-		vector<string>::const_iterator end_attr,const vector<string> & labelVec) {
+		vector<string>::const_iterator end_attr,
+		const vector<string> & labelVec) {
 	map<string, map<string, int> > countAttr; //记录属性中每个不同之对应的不同标签的个数
 	int lineNum = 0;
 	for (vector<string>::const_iterator it = beg_attr; it != end_attr; it++) {
@@ -88,20 +75,30 @@ double calcOneAttrEntropy(vector<string>::const_iterator beg_attr,
 	}
 	return returnEntropy;
 }
-
-
-int computeMaxEntropyPoint(vector<vector<string> >::const_iterator beg,
-		vector<vector<string> >::const_iterator end,
-		const vector<string> &labelVec) {
+//计算熵增最大的点
+int computeMaxEntropyPoint(const vector<vector<string> >&pdataVec,
+		const vector<string> &plabelVec,set<int>&readLineNum,vector<int>&readColumnNum) {
+	vector<string> labelVec;
+	vector<vector<string> > dataVec;
+	for(set<int>::const_iterator lineNum = readLineNum.begin();lineNum!=readLineNum.end();lineNum++)
+		labelVec.push_back(plabelVec[*lineNum]);
+	for(vector<int>::const_iterator col = readColumnNum.begin();col!=readColumnNum.end();col++){
+		vector<string> columnData;
+		for(set<int>::const_iterator lineNum = readLineNum.begin();lineNum!=readLineNum.end();lineNum++)
+			columnData.push_back(pdataVec[*col][*lineNum]);
+		dataVec.push_back(columnData);
+		columnData.clear();
+	}
 	vector<double> label_pro;
-	calcPro(labelVec.begin(),labelVec.end(),&label_pro);
-	double infos = calcEntropy(label_pro.begin(),label_pro.end());
+	calcPro(labelVec.begin(), labelVec.end(), &label_pro);
+	double infos = calcEntropy(label_pro.begin(), label_pro.end());
 	double max_ent = 0.0;
 	int max_index = 0;
 	int i = 0;
-	for (vector<vector<string> >::const_iterator it = beg; it != end; it++) {
+	for (vector<vector<string> >::const_iterator it = dataVec.begin(); it != dataVec.end(); it++) {
 		double ent = calcOneAttrEntropy(it->begin(), it->end(), labelVec);
-		double gain = infos-ent;
+		//cout << "ent" << i << " = " << ent << endl;
+		double gain = infos - ent;
 		if (gain > max_ent) {
 			max_ent = gain;
 			max_index = i;
@@ -110,42 +107,104 @@ int computeMaxEntropyPoint(vector<vector<string> >::const_iterator beg,
 	}
 	return max_index;
 }
-
-//划分属性，并删除原有属性数据，释放内存
-void splitAttr(vector<vector<string> >::iterator beg,
-		vector<vector<string> >::iterator end, vector<int> &symList,
-		vector<vector<string> > *result) {
-	int i = 0;
-	for (vector<vector<string> >::iterator iter = beg; iter != end; iter++) {
-		if (find(symList.begin(), symList.end(), i) == symList.end()) {
-			vector<string> dataSplit;
-			splitAttrTwo(iter->begin(), iter->end(), &dataSplit);
-			result->push_back(dataSplit);
-		} else {
-			vector<string> dataSplit(iter->begin(), iter->end());
-			result->push_back(dataSplit);
+//查找出现次数最多的标签，如果第二多的标签数小于给的阈值，返回次数最多的标签，否则返回空
+string mostLabel(map<string, int>::const_iterator beg,
+		map<string, int>::const_iterator end, int threshold) {
+	int max = 0;
+	string max_label = "";
+	int second = 0;
+	string second_label = "";
+	for (map<string, int>::const_iterator it = beg; it != end; it++) {
+		if (it->second > second) {
+			second = it->second;
+			second_label = it->first;
 		}
-		i++;
-		iter->clear(); //清除原有数据，释放内存
+		if (second > max) {
+			int tmp = max;
+			max = second;
+			second = tmp;
+			max_label = second_label;
+		}
 	}
+	if (second > threshold)
+		max_label = "";
+	return max_label;
+}
+
+void CreateTree(TreeNode*treeHead, vector<vector<string> >&dataVec,
+		vector<string>&labelVec, set<int> &readLineNum,
+		vector<int> &readColumnNum, const int &threshold) {
+	if (readLineNum.size() == 0)
+		return;
+	cout << "lineNum = " << readLineNum.size() << endl;
+	int attr_node = computeMaxEntropyPoint(dataVec, labelVec,readLineNum,readColumnNum);
+	readColumnNum[attr_node] = 1; //该点作为分节点
+	TreeNode *treeNode = new TreeNode();
+	treeNode->h_iAttribution = attr_node; //记录节点位置
+	countLabel(labelVec,readLineNum.begin(),readLineNum.end(), &treeNode->h_mLabelNum); //统计每个标签出现次数
+	treeNode->h_sLabel = mostLabel(treeNode->h_mLabelNum.begin(),
+			treeNode->h_mLabelNum.end(), threshold); //寻找最大值节点
+	if (treeHead == NULL)
+		treeHead = treeNode;
+	else {
+		treeNode->h_vChildren.push_back(treeNode);
+	}
+	if (treeNode->h_sLabel == "") { //标签为空，说明不是叶子节点，递归建树
+		map<string,set<int> > newReadLineNum;
+		vector<string>::const_iterator iter_end = dataVec[attr_node].end();
+		int i = 0;
+		for(vector<string>::const_iterator iter=dataVec[attr_node].begin();iter!=iter_end;iter++,i++){
+			newReadLineNum[*iter].insert(i);
+		}
+		map<string,set<int> >::iterator iter_end_map = newReadLineNum.end();
+		for(map<string,set<int> >::iterator iter = newReadLineNum.begin();iter!=iter_end_map;iter++){
+			CreateTree(treeNode,dataVec,labelVec,iter->second,readColumnNum,threshold);
+		}
+	}
+//	readColumnNum[attr_node] = 1;
 }
 
 int main() {
-	string tf =
-			"/home/huaa/workspace/Data_Mining_NB/naive_bayes/kddcup.data_10_percent.txt"; //train_file
-	string pf = "/home/huaa/workspace/Data_Mining_NB/naive_bayes/corrected"; //predict_file
+	string tf = "/home/huaa/workspace/kddcup.data_10_percent.txt"; //train_file
+	string pf = "/home/huaa/Source_File/Data_Mining/corrected"; //predict_file
 
 	int a[7] = { 1, 2, 3, 6, 11, 20, 21 }; //数据部分离散变量位置
 	vector<int> symbolicList(a, a + 7);
 	vector<vector<string> > dataVec;
 	vector<string> labelVec;
 	vector<double> pro_label;
+	cout << "读取数据..." << endl;
 	if (!readfile_with_column(tf, &dataVec, &labelVec))
 		return 0; //按列读取文件
-	vector<vector<string> > result;
-	splitAttr(dataVec.begin(), dataVec.end(), symbolicList, &result);
-	int index = computeMaxEntropyPoint(result.begin(),result.end(),labelVec);
-	cout << "index = " << index << endl;
+	cout << "数据读入完成." << endl;
+	cout << "数据离散化..." << endl;
+	vector<vector<string> > splitData;
+	splitAttr(dataVec.begin(), dataVec.end(), symbolicList, &splitData);
+	cout << "数据离散化完成." << endl;
+	cout << "开始建立决策树..." << endl;
+	TreeNode *HeadTree = NULL;
+	set<int> readLineNum;
+	for(int i = 0;i<TOTAL_LINE;i++)
+		readLineNum.insert(i);
+	vector<int>readColumnNum;
+	for(int i = 0;i<ATTRIBUTION_NUM;i++)
+		readColumnNum.push_back(i);
+	int threshold = 1000;
+	CreateTree(HeadTree,splitData,labelVec,readLineNum,readColumnNum,threshold);
+	cout << "决策树建立完成." << endl;
+	cout << "打印树..." << endl;
+	printTree(HeadTree);
+
+//	map<string, int> tmostLabel;
+//	string str[5] = { "a", "b", "c", "d", "e" };
+//	int num[5] = { 10, 20, 13, 300, 40 };
+//	for (int i = 0; i < 5; i++)
+//		tmostLabel[str[i]] = num[i];
+//	string res_str = mostLabel(tmostLabel.begin(), tmostLabel.end(), 80);
+//	cout << res_str << endl;
+
+//	int index = computeMaxEntropyPoint(result.begin(),result.end(),labelVec);
+//	cout << "index = " << index << endl;
 
 //	int i = 0;
 //	double max_ent = 0.0;
